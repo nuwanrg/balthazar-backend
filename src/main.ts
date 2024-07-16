ec16d7d5d1e65436d46b2ba24fbf561333f1cf61
nuwanrg <nuwan_rajitha@outlook.com>
2024-07-16 17:34:32 +0530

    Enable CORS for any origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // Enable CORS for any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -19,24 +19,39 @@ export class NftService {
     this.OS_API_KEY = this.configService.get<string>('OS_API_KEY');
   }
 
+  /**
+   * Fetch NFT data for a given owner address with optional query parameters.
+   *
+   * @param ownerAddress - The address of the NFT owner.
+   * @param collection - (Optional) The collection to filter the NFTs.
+   * @param limit - (Optional) The maximum number of NFTs to return.
+   * @param next - (Optional) The token for pagination to fetch the next set of NFTs.
+   * @returns An Observable of AxiosResponse containing the NFT data.
+   */
   getNftData(
     ownerAddress: string,
     collection?: string,
     limit?: number,
     next?: string,
   ): Observable<AxiosResponse<any>> {
+    // Construct the URL with the owner address
     const url = `${this.OS_GET_NFT_URL}/${ownerAddress}/nfts`;
+
+    // Set the headers including the API key
     const headers = {
       accept: 'application/json',
       'x-api-key': this.OS_API_KEY,
     };
 
+    // Initialize the query parameters object
     const params: any = {};
     if (collection) params.collection = collection;
     if (limit) params.limit = limit;
     if (next) params.next = next;
 
+    // Make the HTTP GET request with the constructed URL, headers, and parameters
     return this.httpService.get(url, { headers, params }).pipe(
+      // Handle any errors that occur during the request
       catchError((error) => {
         console.error('Error fetching NFT data:', error);
         return throwError(() => new Error('Error fetching NFT data'));
