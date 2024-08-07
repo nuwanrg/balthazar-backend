0b0df89e1a33ae6f6350916bea86688d8db71c20
nuwanrg <nuwan_rajitha@outlook.com>
2024-08-07 17:18:11 +0530

    Enable redis

--- src/nft/nft.controller.ts
+++ src/nft/nft.controller.ts
@@ -5,24 +5,24 @@ import { NftService } from './nft.service';
 export class NftController {
   constructor(private readonly nftService: NftService) {}
 
-  @Get('data')
-  async getNftData(
-    @Query('owner') owner: string,
-    @Query('collection') collection?: string,
-    @Query('limit') limit?: number,
-    @Query('next') next?: string,
-  ) {
-    try {
-      const response = await this.nftService
-        .getNftData(owner, collection, limit, next)
-        .toPromise();
-      return response.data;
-    } catch (error) {
-      throw new Error(`Failed to fetch NFT data: ${error.message}`);
-    }
-  }
+  // @Get('data')
+  // async getNftData(
+  //   @Query('owner') owner: string,
+  //   @Query('collection') collection?: string,
+  //   @Query('limit') limit?: number,
+  //   @Query('next') next?: string,
+  // ) {
+  //   try {
+  //     const response = await this.nftService
+  //       .getNftData(owner, collection, limit, next)
+  //       .toPromise();
+  //     return response.data;
+  //   } catch (error) {
+  //     throw new Error(`Failed to fetch NFT data: ${error.message}`);
+  //   }
+  // }
 
-  @Get('dataWithCache')
+  @Get('data')
   async getNftDataWithCache(
     @Query('owner') owner: string,
     @Query('collection') collection?: string,
